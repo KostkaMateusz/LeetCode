bd5a23f279ca9c13c96e68a3fd6ced12c0becdfd
Mateusz Kostka <mateuszkostka5@gmail.com>
2023-12-22 14:47:09 +0100

    1816

---
+++ 1816_TruncateSentence/Program.cs
@@ -0,0 +1,16 @@
+﻿//https://leetcode.com/problems/truncate-sentence/description/
+
+var sol = new Solution();
+
+var st=sol.TruncateSentence("Hello how are you Contestant",4);
+
+Console.WriteLine(st);
+public class Solution
+{
+    public string TruncateSentence(string s, int k)
+    {
+        var output= s.Split(" ").Take(k).ToArray();
+
+        return string.Join(" ",output);
+    }
+}
