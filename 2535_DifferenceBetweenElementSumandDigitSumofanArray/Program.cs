bc2ea1e1790643cd2e077f8f905091a1e54b6a86
Mateusz Kostka <mateuszkostka5@gmail.com>
2023-12-27 14:29:39 +0100

    2535_DifferenceBetweenElementSumandDigitSumofanArray

---
+++ 2535_DifferenceBetweenElementSumandDigitSumofanArray/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+var sol = new Solution();
+
+sol.DifferenceOfSum([1, 15, 6, 3]);
+
+
+public class Solution
+{
+    public int DifferenceOfSum(int[] nums)
+    {
+        var sumDiference = ElementSum(nums);
+        sumDiference-=DigitSum(nums);
+        return sumDiference;
+    }
+
+    private int ElementSum(int[] nums)
+    {
+        int sum = 0;        
+        for (int i = 0; i < nums.Length; i++)
+            sum+= nums[i];
+    
+        return sum;
+    }
+
+    private int DigitSum(int[] nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var currentElement = nums[i];
+            while (currentElement != 0)
+            {                
+                sum += currentElement % 10;
+                currentElement /= 10;
+            }
+        }
+        return sum;
+    }
+
+}
