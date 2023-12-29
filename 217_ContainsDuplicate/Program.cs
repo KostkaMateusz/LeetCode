b7b4ecc2e60b38ccdd0e85b79a4231a7819287fc
Mateusz Kostka <mateuszkostka5@gmail.com>
2023-12-29 15:07:15 +0100

    217. Contains Duplicate

--- 217_ContainsDuplicate/Program.cs
+++ 217_ContainsDuplicate/Program.cs
@@ -5,16 +5,26 @@ public class Solution
 {
     public bool ContainsDuplicate(int[] nums)
     {
+        // Create a HashSet to store unique integers
         var set = new HashSet<int>();
 
+        // Iterate through each integer 'x' in the input array 'nums'
         foreach (int x in nums)
         {
+            // Check if the HashSet already contains the current integer 'x'
             if (set.Contains(x))
             {
+                // If 'x' is already in the HashSet, there is a duplicate
+                // Return true indicating the presence of a duplicate
                 return true;
             }
+
+            // If 'x' is not in the HashSet, add it to the HashSet
             set.Add(x);
         }
+
+        // If the loop completes without finding any duplicates, return false
         return false;
     }
+
 }
