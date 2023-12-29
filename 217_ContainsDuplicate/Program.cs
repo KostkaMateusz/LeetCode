c717ff3d78701723df92ddb9f69bdc88cd7029cf
Mateusz Kostka <mateuszkostka5@gmail.com>
2023-12-29 15:01:58 +0100

    217. Contains Duplicate

---
+++ 217_ContainsDuplicate/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var set = new HashSet<int>();
+
+        foreach (int x in nums)
+        {
+            if (set.Contains(x))
+            {
+                return true;
+            }
+            set.Add(x);
+        }
+        return false;
+    }
+}
