545b878c2c5b9a7c794357ddc6503e0d6347e466
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-01-11 17:33:53 +0100

    167. Two Sum II - Input Array Is Sorted

---
+++ 167_TwoSumIIInputArrayIsSorted/Program.cs
@@ -0,0 +1,31 @@
+﻿//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+var sol = new Solution();
+
+sol.TwoSum(numbers: [2, 7, 11, 15], target: 9);
+sol.TwoSum(numbers: [2, 3, 4], target: 6);
+sol.TwoSum(numbers: [-1, 0], target: -1);
+sol.TwoSum(numbers: [0, 0, 3, 4], target: 0);
+
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int currentSum = 0;
+        int leftPointer = 0;
+        int rightPointer = numbers.Length - 1;
+
+        do
+        {
+            currentSum = numbers[leftPointer] + numbers[rightPointer];         
+            if (currentSum < target)
+                leftPointer++;
+            else if (currentSum > target)
+                rightPointer--;
+        }
+        while (currentSum != target);
+
+        return [leftPointer + 1, rightPointer + 1];
+    }
+}
