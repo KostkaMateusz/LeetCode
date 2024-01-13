04bd7cdeab7c3bd6a05a6f1dc35229539b1f5c81
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-01-13 23:43:03 +0100

    11. Container With Most Water

---
+++ 11_ContainerWithMostWater/Program.cs
@@ -0,0 +1,31 @@
+﻿//https://leetcode.com/problems/container-with-most-water/description/
+
+var sol = new Solution();
+
+var m = sol.MaxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
+Console.WriteLine(m);
+
+
+Console.WriteLine(sol.MaxArea([1, 1]));
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        int pointerA = 0;
+        int pointerB = height.Length - 1;
+
+        while (pointerB>pointerA)
+        {                
+            int area = Math.Min(height[pointerA], height[pointerB]) * (pointerB - pointerA);
+            maxArea = Math.Max(maxArea, area);
+
+            if (height[pointerA] < height[pointerB])            
+                pointerA++;            
+            else            
+                pointerB--;
+        }
+        return maxArea;
+    }
+}
