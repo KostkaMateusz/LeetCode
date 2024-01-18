a4a63bd43552a9e676792fdc2dfc8981d88ad80d
unknown <mateuszkostka5@gmail.com>
2024-01-18 17:41:34 +0000

    35. Search Insert Position

---
+++ 35_SearchInsertPosition/Program.cs
@@ -0,0 +1,36 @@
+﻿//https://leetcode.com/problems/binary-search/
+
+var sol = new Solution();
+int index;
+
+index = sol.SearchInsert(nums : [1, 3, 5, 6], target : 5);
+Console.WriteLine(index);
+
+index = sol.SearchInsert(nums: [1, 3, 5, 6], target : 2);
+Console.WriteLine(index);
+
+
+index = sol.SearchInsert(nums : [1, 3, 5, 6], target : 7);
+Console.WriteLine(index);
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int low = 0;
+        int high = nums.Length - 1;
+        
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (target == nums[mid])
+                return mid;
+            if (target > nums[mid])
+                low = mid + 1;           
+            else
+                high = mid - 1;           
+        }
+        return low;
+    }
+}
