6cc6f8c0ee20b1a5c920b74529a4e7c667e4c306
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-01-22 00:54:08 +0100

    58. Length of Last Word

---
+++ 58_LengthofLastWord/Program.cs
@@ -0,0 +1,34 @@
+﻿//https://leetcode.com/problems/length-of-last-word/
+
+var sol = new Solution();
+
+Console.WriteLine(sol.LengthOfLastWord("Hello World"));
+Console.WriteLine(sol.LengthOfLastWord("   fly me   to   the moon  "));
+Console.WriteLine(sol.LengthOfLastWord("luffy is still joyboy"));
+Console.WriteLine(sol.LengthOfLastWord("a"));
+Console.WriteLine(sol.LengthOfLastWord(" a "));
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        char[] characters = s.ToCharArray();
+
+        int characterCount = 0;
+        int pointer = s.Length - 1;
+
+        //filter non letters from end
+        while (pointer >= 0 && Char.IsWhiteSpace(characters[pointer]))
+        {
+            pointer--;
+        }
+
+        //count letters
+        while (pointer >= 0 && Char.IsLetterOrDigit(characters[pointer]))
+        {
+            characterCount++;
+            pointer--;
+        }
+        return characterCount;
+    }
+}
