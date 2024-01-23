f7da2feb1fe4e36009d7f7a6df390f85544e1e50
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-01-23 23:46:53 +0100

    344. Reverse String

---
+++ 344_ReverseString/Program.cs
@@ -0,0 +1,29 @@
+﻿//https://leetcode.com/problems/reverse-string/
+
+var sol = new Solution();
+
+char[] array = ['h'];
+
+sol.ReverseString(array);
+
+
+Console.WriteLine(array);
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int leftPointer = 0;
+        int rightPointer = s.Length-1;
+
+        while (leftPointer <= rightPointer)
+        {
+            var rightTemp = s[rightPointer];
+            s[rightPointer] = s[leftPointer];
+            s[leftPointer]=rightTemp;
+
+            leftPointer++;
+            rightPointer--;
+        }       
+    }
+}
