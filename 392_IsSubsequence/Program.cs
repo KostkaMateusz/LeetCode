69f7f55190df8c68f351ad41f00d0a5a0b05126a
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-01-25 20:49:52 +0100

    392. Is Subsequence

---
+++ 392_IsSubsequence/Program.cs
@@ -0,0 +1,33 @@
+﻿//https://leetcode.com/problems/is-subsequence/
+
+var sol = new Solution();
+
+bool sub;
+
+sub=sol.IsSubsequence(s : "abc", t : "ahbgdc");
+Console.WriteLine(sub);
+
+sub = sol.IsSubsequence(s : "axc", t : "ahbgdc");
+Console.WriteLine(sub);
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        var tPointer = 0;
+        var sPointer = 0;
+
+        while(tPointer<t.Length && sPointer < s.Length)
+        {
+            if (s[sPointer] == t[tPointer])            
+                sPointer++;
+            
+            tPointer++;
+        }
+
+        if (sPointer == s.Length)        
+            return true;        
+        else        
+            return false;        
+    }
+}
