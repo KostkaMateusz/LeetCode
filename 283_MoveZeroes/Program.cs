fa3a1b41abf6e1117b1a46b10606a33dc00730d6
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-01-27 13:13:13 +0100

    283. Move Zeroes

---
+++ 283_MoveZeroes/Program.cs
@@ -0,0 +1,28 @@
+﻿//https://leetcode.com/problems/move-zeroes/description/
+
+var sol = new Solution();
+
+sol.MoveZeroes([0, 1, 0, 3, 12]);
+
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int leftPointer = 0;
+        int rightPointer = 0;
+
+        while (rightPointer < nums.Length)
+        {
+            if (nums[rightPointer] != 0)
+            {
+                int temp = nums[leftPointer];
+                nums[leftPointer] = nums[rightPointer];
+                nums[rightPointer] = temp;
+
+                leftPointer++;
+            }
+            rightPointer++;
+        }
+    }
+}
