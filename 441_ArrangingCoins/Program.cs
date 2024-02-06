b014f644920881e14532bedbda7d3ec9cd5407a0
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-02-06 21:46:33 +0100

    441. Arranging Coins

---
+++ 441_ArrangingCoins/Program.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.com/problems/arranging-coins/description/
+
+var sol = new Solution();
+
+sol.ArrangeCoins(5);
+
+public class Solution
+{
+    public int ArrangeCoins(int n)
+    {
+        var left = 0;
+        var right = n;
+        var result = 0;
+
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+            //Gauss equation
+            var coins = ((double)mid / 2) * (mid + 1);
+
+            if (coins > n)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+                result = Math.Max(mid, result);
+            }
+        }
+
+        return result;
+    }
+}
