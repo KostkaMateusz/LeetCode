04cbd03c7a3b9fb98b1f502696768fdfbeabf3ca
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-02-08 21:33:42 +0100

    14. Longest Common Prefix

---
+++ 14_LongestCommonPrefix/Program.cs
@@ -0,0 +1,38 @@
+﻿// https://leetcode.com/problems/longest-common-prefix/
+
+using System.Text;
+
+var sol = new Solution();
+
+sol.LongestCommonPrefix(["flower", "flow", "flight"]);
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var preFixCount = new int[strs[0].Length];
+        Array.Fill(preFixCount, 0);
+        var stringZero = strs[0];
+
+        foreach (var word in strs)
+        {
+            for (var i = 0; i < stringZero.Length && i < word.Length; i++)
+            {
+                if (word[i] == stringZero[i])
+                    preFixCount[i]++;
+                else
+                    break;
+            }
+        }
+        
+        int elements = 0;
+        foreach (var t in preFixCount)
+        {
+            if (t ==  strs.Length)
+                elements++;
+            else
+                break;
+        }
+        return stringZero[..elements];
+    }
+}
