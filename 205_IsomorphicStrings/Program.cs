eabbb1c6a3008deb5c76ba72038111e219ff476f
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-02-10 15:24:07 +0100

    205. Isomorphic Strings

---
+++ 205_IsomorphicStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.com/problems/isomorphic-strings/
+
+var sol = new Solution();
+sol.IsIsomorphic("egg", "add");
+
+
+
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        Dictionary<char, char> characterMap = new();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (characterMap.TryGetValue(s[i], out var letter))
+            {
+                if (letter != t[i])
+                {
+                    return false;
+                }
+                continue;
+            }
+
+            if (characterMap.ContainsValue(t[i]))
+            {
+                return false;
+            }
+            
+            characterMap.Add(s[i], t[i]);
+        }
+        return true;
+    }
+}
