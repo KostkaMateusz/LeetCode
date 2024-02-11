95ad205f30936c54837c08f20b77b8d98f60b0a4
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-02-11 20:50:08 +0100

    605. Can Place Flowers

---
+++ 605_CanPlaceFlowers/Program.cs
@@ -0,0 +1,33 @@
+﻿// https://leetcode.com/problems/can-place-flowers/description/
+
+var sol = new Solution();
+
+var res = sol.CanPlaceFlowers([1, 0, 0, 0, 1], 1);
+Console.WriteLine(res);
+
+res = sol.CanPlaceFlowers([1, 0, 0, 0, 1], 2);
+Console.WriteLine(res);
+
+
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        for (var i = 0; i < flowerbed.Length; i++)
+        {
+            if(n == 0) 
+                return true;
+
+            var prevSpot = i - 1 < 0 ? 0 : flowerbed[i - 1];
+
+            var nextSpot = i + 1 >= flowerbed.Length ? 0 : flowerbed[i + 1];
+
+            if (prevSpot == 0 && flowerbed[i] == 0 && nextSpot == 0)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n==0;
+    }
+}
