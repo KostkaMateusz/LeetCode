ac578a82d61ef71cb6fd3d9f94ccf4f795649aac
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-02-14 23:24:05 +0100

    28. Find the Index of the First Occurrence in a String

---
+++ 28_FindtheIndexoftheFirstOccurrenceinaString/Program.cs
@@ -0,0 +1,10 @@
+﻿// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+
+public class Solution 
+{
+    public int StrStr(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle);
+    }
+}
