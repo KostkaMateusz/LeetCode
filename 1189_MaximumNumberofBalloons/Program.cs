7567c390676bcae58e3d6aa3db100affa8e1d90a
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-02-14 22:56:32 +0100

    1189. Maximum Number of Balloons

---
+++ 1189_MaximumNumberofBalloons/Program.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.com/problems/maximum-number-of-balloons/
+
+var sol = new Solution();
+
+Console.WriteLine(sol.MaxNumberOfBalloons("nlaebolko"));
+Console.WriteLine(sol.MaxNumberOfBalloons("loonbalxballpoon"));
+
+public class Solution 
+{
+    public int MaxNumberOfBalloons(string text)
+    {
+        Dictionary<char, int> ballonLetters = new()
+            {
+                {'b',0},
+                {'a',0},
+                {'l',0},
+                {'o',0},
+                {'n',0}
+            };
+
+        foreach (var letter in text)
+        {
+            if (ballonLetters.ContainsKey(letter))
+            {
+                ballonLetters[letter]++;
+            }
+        }
+        
+        ballonLetters['l'] /= 2;
+        ballonLetters['o'] /= 2;
+
+        return ballonLetters.Values.Min();
+    }
+}
