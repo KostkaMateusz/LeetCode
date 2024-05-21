5c8d19d230c7261ec292c1cacb6b1708a1ec1692
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-05-21 22:56:09 +0200

    27. Remove Element

---
+++ 27_RemoveElement/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://leetcode.com/problems/remove-element/
+
+
+var sol = new Solution();
+// sol.RemoveElement(nums: [0, 1, 2, 2, 3, 0, 4, 2], val: 2);
+sol.RemoveElement(nums: [1], val: 1);
+
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;  // Counter for the new length
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
