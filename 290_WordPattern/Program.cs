010bfea3bc0cccdd4c6fa3ab64c6d916c6de70ee
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-06-04 23:09:02 +0200

    290. Word Pattern

---
+++ 290_WordPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿var sol = new Solution();
+
+
+
+Console.WriteLine(sol.WordPattern(pattern: "abba", s: "dog cat cat dog"));
+Console.WriteLine(sol.WordPattern(pattern: "abba", s: "dog dog dog dog"));
+
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        var words = s.Split(' ');
+
+        if (words.Length != pattern.Length)
+            return false;
+
+        var patternToWord = new Dictionary<char, string>();
+        var wordToPattern = new Dictionary<string, char>();
+     
+        
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            if (!patternToWord.TryGetValue(pattern[i], out string? value))
+            {
+                patternToWord[pattern[i]] = words[i];
+            }
+            else if (value != words[i])
+            {
+                return false;
+            }
+
+
+
+            patternToWord.TryAdd('a', "b");
+            patternToWord.TryAdd('a', "c");
+
+            patternToWord['a'];
+            
+            if (!wordToPattern.TryGetValue(words[i], out char valuePattern))
+            {
+                wordToPattern[words[i]] = pattern[i];
+            }
+            else if (valuePattern != pattern[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ 496_NextGreaterElementI/Program.cs
@@ -0,0 +1,35 @@
+﻿var sol = new Solution();
+
+sol.NextGreaterElement(nums1 : [4,1,2], nums2 : [1,3,4,2]);
+
+public class Solution
+{
+    public int[] NextGreaterElement(int[] nums1, int[] nums2)
+    {
+        var lookUpTable = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums2.Length; i++)
+        {
+            for (int j = i; j < nums2.Length; j++)
+            {
+                if (nums2[j] > nums2[i])
+                {
+                    lookUpTable[nums2[i]] = nums2[j];
+                    break;
+                }
+            }
+
+            if (!lookUpTable.ContainsKey(nums2[i]))
+                lookUpTable[nums2[i]] = -1;
+        }
+
+        var output = new int[nums1.Length];
+
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            output[i] = lookUpTable[nums1[i]];
+        }
+
+        return output;
+    }
+}
