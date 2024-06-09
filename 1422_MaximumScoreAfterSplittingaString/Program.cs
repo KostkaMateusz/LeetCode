39ec9a00289f988ae552112dac37857e51ab4d54
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-06-09 19:17:31 +0200

    1422. Maximum Score After Splitting a String

---
+++ 1422_MaximumScoreAfterSplittingaString/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+var sol = new Solution();
+
+
+
+Console.WriteLine(sol.MaxScore("011101"));
+
+
+Console.WriteLine(sol.MaxScore("00111"));
+
+
+public class Solution
+{
+    public int MaxScore(string s)
+    {
+        var zero = 0;
+        var one = s.Count(x => x == '1');
+        var result = 0;
+
+
+        foreach(var x in s)
+        {
+            if (x == '0')
+            {
+                zero++;
+            }
+            else
+            {
+                one--;
+            }
+            result=Math.Max(result, zero+one);
+        }
+        return result;
+    }
+}
