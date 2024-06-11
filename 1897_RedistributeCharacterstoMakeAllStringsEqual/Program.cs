720c7c8d83414d821f99131bfcbaaea24555b4e2
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-06-11 20:59:37 +0200

    1897. Redistribute Characters to Make All Strings Equal

---
+++ 1897_RedistributeCharacterstoMakeAllStringsEqual/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+var sol = new Solution();
+
+
+Console.WriteLine(sol.MakeEqual(["abc", "aabc", "bc"]));
+
+public class Solution
+{
+    public bool MakeEqual(string[] words)
+    {
+        var hashMap=new Dictionary<char, int>();
+
+        foreach (var word in words)
+        {
+            foreach(var letter in word)
+            {
+                if(hashMap.ContainsKey(letter))
+                {
+                    hashMap[letter]++;
+                }
+                else
+                {
+                    hashMap.Add(letter, 1);
+                }
+            }
+        }
+
+        foreach(var count in hashMap.Values)
+        {
+            if(count % words.Length != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
