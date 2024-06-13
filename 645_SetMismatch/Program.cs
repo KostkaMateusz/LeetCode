5df957918d237d6735ad13a2a2ab5f96b545c721
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-06-13 21:41:07 +0200

    645. Set Mismatch

---
+++ 645_SetMismatch/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+var sol = new Solution();
+
+sol.FindErrorNums([1, 2, 2, 4]);
+
+public class Solution
+{
+    public int[] FindErrorNums(int[] nums)
+    {
+        var hashMap=new HashSet<int>();
+
+        var n=nums.Length;
+
+        int doubled=0;
+        int missing=0;
+
+        for (int i=0; i<n; i++)
+        {
+            if (hashMap.Contains(nums[i]))
+            {
+                doubled = nums[i];              
+            }      
+            
+            hashMap.Add(nums[i]);            
+        }
+
+        for(int i = 1; i <= n; i++)
+        {
+            if (!hashMap.Contains(i))
+            {
+                missing = i;
+            }
+        }
+
+        return new int[] { doubled, missing };
+    }
+}
