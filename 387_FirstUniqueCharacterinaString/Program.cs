aad73bba3d6f929ab2df617105c73e4608da05e1
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-06-14 21:10:19 +0200

    387. First Unique Character in a String

---
+++ 387_FirstUniqueCharacterinaString/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+
+var sol = new Solution();
+
+sol.FirstUniqChar("loveleetcode");
+
+
+
+public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        var HashMap = new Dictionary<char, List<int>>();
+
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (HashMap.TryGetValue(s[i], out var value))
+            {
+                value.Add(i);
+            }
+            else
+            {
+                HashMap.Add(s[i], [i]);
+            }
+
+        }
+
+        var uniqueIndices = HashMap.Values.Where(x => x.Count() == 1).Select(x => x[0]);
+
+        return uniqueIndices.Any() ? uniqueIndices.Min() : -1;
+
+    }
+}
