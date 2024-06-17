ebca0e265e5c9ae77a469bd4b4cde20c2d3dd137
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-06-17 20:32:14 +0200

    1700. Number of Students Unable to Eat Lunch

---
+++ 1700_NumberofStudentsUnabletoEatLunch/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+using System.Linq;
+
+var sol = new Solution();
+
+sol.CountStudents(students : [1, 1, 0, 0], sandwiches : [0, 1, 0, 1]);
+
+
+public class Solution
+{
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        var studentsCounter = students.GroupBy(x => x).ToDictionary(x =>  x.Key , x=>x.Count() );
+        var numberOfSandwiches= sandwiches.Length;
+
+        foreach(var sand in sandwiches)
+        {
+            if (studentsCounter.TryGetValue(sand, out int value) && value>0)
+            {
+                if (value > 0)
+                {
+                    studentsCounter[sand]--;
+                    numberOfSandwiches--;
+                }
+            }
+            else
+                return numberOfSandwiches;
+        }
+
+        return numberOfSandwiches; 
+    }
+}
