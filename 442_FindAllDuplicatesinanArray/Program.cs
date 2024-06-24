595164d945b08a2c7f0218b03ab19e924e91c6f3
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-06-24 20:23:25 +0200

    442. Find All Duplicates in an Array

---
+++ 442_FindAllDuplicatesinanArray/Program.cs
@@ -0,0 +1,53 @@
+﻿
+var sol = new Solution();
+
+sol.FindDuplicates([4, 3, 2, 7, 8, 2, 3, 1]);
+
+
+
+public class Solution
+{
+    public IList<int> FindDuplicates(int[] nums)
+    {
+        var output = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var num = Math.Abs(nums[i]);
+
+            if (nums[num - 1] <= 0)
+            {
+                output.Add(num);
+            }
+
+            nums[num - 1] = nums[num - 1] * (-1);
+        }
+        return output;
+    }
+}
+
+
+
+
+public class Solution_Breaking_Space_Constrain
+{
+    public IList<int> FindDuplicates(int[] nums)
+    {
+
+        var map = new int[nums.Length+1];
+        var output = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            map[nums[i]]++;
+            if (map[nums[i]] == 2)
+            {
+                output.Add(nums[i]);
+            }
+
+        }
+        return output;
+
+
+    }
+}
