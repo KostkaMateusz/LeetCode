ac65a19dc60f40c72df931c01c185b948e224035
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-11-21 22:58:37 +0100

    242. Valid Anagram

---
+++ 242_Valid_Anagram/Program.cs
@@ -0,0 +1,36 @@
+﻿// https://leetcode.com/problems/valid-anagram/description/
+
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        Dictionary<char, int> sMap = new();
+
+        foreach (var character in s)
+        {
+            if (sMap.TryAdd(character, 1) is false)
+                sMap[character]++;
+        }
+
+        foreach (var character in t)
+        {
+            if (sMap.ContainsKey(character))
+            {
+                sMap[character]--;
+            }
+            else
+            {
+                return false;
+            }
+
+            if (sMap[character] < 0)
+                return false;
+        }
+
+        return true;
+    }
+}
