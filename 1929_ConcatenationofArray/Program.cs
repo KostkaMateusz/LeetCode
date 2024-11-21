9c2a2363a5c5b2f5eb1bb36e5216c1bd00ebe7bf
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-11-21 23:10:16 +0100

    1929. Concatenation of Array

---
+++ 1929_ConcatenationofArray/Program.cs
@@ -0,0 +1,10 @@
+﻿// https://leetcode.com/problems/concatenation-of-array/
+
+
+public class Solution
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        return [..nums, ..nums];
+    }
+}
