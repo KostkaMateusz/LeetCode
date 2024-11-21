8d7df452feb6a97cdd3d895075e5ff339936942e
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-11-21 22:35:27 +0100

    217. Contains Duplicate

---
+++ 217_ContainsDuplicate/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://leetcode.com/problems/contains-duplicate/
+
+var solution = new Solution();
+
+var output = solution.ContainsDuplicate([1, 2, 3, 1]);
+
+Console.WriteLine(output);
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> numsSet = [];
+
+        foreach (var num in nums)
+        {
+            if (numsSet.Add(num) is false)
+                return true;
+        }
+
+        return false;
+    }
+}
