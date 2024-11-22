ab6a72fadfeb1ff7728e1ee3ef652cd107926781
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-11-22 20:09:26 +0100

    1. Two Sum

---
+++ 1_TwoSum/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/two-sum/
+
+int[] nums = [2, 7, 11, 15];
+
+var target = 9;
+
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var searchFor = target - nums[i];
+
+            if (dict.TryGetValue(searchFor, out int res) is false)
+                dict[nums[i]] = i;
+            else
+            {
+                return [res, i];
+            }
+        }
+
+        return [];
+    }
+}
