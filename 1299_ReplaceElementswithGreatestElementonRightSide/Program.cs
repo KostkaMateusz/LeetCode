c413071ebf993e4c451daa537e33e0823372d277
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-11-22 18:50:28 +0100

    1299. Replace Elements with Greatest Element on Right Side

---
+++ 1299_ReplaceElementswithGreatestElementonRightSide/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+
+
+int[] arr = [17, 18, 5, 4, 6, 1];
+
+var sol = new Solution();
+
+var output = sol.ReplaceElements(arr);
+Console.WriteLine();
+
+public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        int rightMaximum = -1;
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            var newMax = Math.Max(arr[i], rightMaximum);
+
+            arr[i] = rightMaximum;
+
+            rightMaximum = newMax;
+        }
+
+        return arr;
+    }
+}
