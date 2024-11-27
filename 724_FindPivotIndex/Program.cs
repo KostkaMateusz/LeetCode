41d69dbd9cb9ec3be8e7d1b6705df378f4e499cf
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-11-27 19:10:27 +0100

    724. Find Pivot Index

---
+++ 724_FindPivotIndex/Program.cs
@@ -0,0 +1,31 @@
+﻿// https://leetcode.com/problems/find-pivot-index/description/
+
+int[] nums = [1, 7, 3, 6, 5, 6];
+
+var sol=new Solution();
+
+var index=sol.PivotIndex(nums);
+
+Console.WriteLine();
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        int rightSum = nums.Sum();
+        int leftSum = 0;
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            leftSum += nums[i];
+            
+            if (leftSum == rightSum)
+            {
+                return i;
+            }
+            
+            rightSum -= nums[i];
+        }
+
+        return -1;
+    }
+}
