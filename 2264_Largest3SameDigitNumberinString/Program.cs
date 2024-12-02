4bfe4929957843422d04c7a39fc801cdd2ed8d3a
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-02 14:27:06 +0100

    2264_Largest3SameDigitNumberinString

---
+++ 2264_Largest3SameDigitNumberinString/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/largest-3-same-digit-number-in-string/
+
+public class Solution
+{
+    public string LargestGoodInteger(string num)
+    {
+        int max = -1;
+        string maxString = "";
+
+        for (var i = 2; i < num.Length; i++)
+        {
+            if (num[i] == num[i - 1] && num[i] == num[i - 2])
+            {
+                var numberString = num.Substring(i - 2, 3);
+
+                var convertednumber = int.Parse(numberString);
+
+                if (convertednumber > max)
+                {
+                    max = convertednumber;
+                    maxString = numberString;
+                }
+            }
+        }
+
+        return maxString;
+    }
+}
