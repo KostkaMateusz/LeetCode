a09cc3ee648b6d5365502cc4eee448edc77bc003
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-16 13:45:33 +0100

    1422. Maximum Score After Splitting a String

---
+++ 1422_MaximumScoreAfterSplittingaString/Program.cs
@@ -0,0 +1,32 @@
+﻿// https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+
+
+public class Solution
+{
+    public int MaxScore(string s)
+    {
+        int score = 0;
+
+        int zeroScore = 0;
+        int oneScore = s.Count(x => x == '1');
+
+        for (int i=0;i<s.Length-1;i++)
+        {
+
+            var element = s[i];
+            
+            if (element == '0')
+            {
+                zeroScore++;
+            }
+            else
+            {
+                oneScore--;
+            }
+
+            score = Math.Max(score, zeroScore + oneScore);
+        }
+
+        return score;
+    }
+}
