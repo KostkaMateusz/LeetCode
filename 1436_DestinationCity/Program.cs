c3227569df7cfb4c5b3e96183738fa169897ee03
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-16 12:57:37 +0100

    1436. Destination City

---
+++ 1436_DestinationCity/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string DestCity(IList<IList<string>> paths)
+    {
+        var source=new HashSet<string>();
+        var destination=new HashSet<string>();
+
+        foreach (var destinationPair in paths)
+        {
+            source.Add(destinationPair[0]);
+            destination.Add(destinationPair[1]);
+        }
+
+        return destination.Except(source).First();
+
+    }
+}
