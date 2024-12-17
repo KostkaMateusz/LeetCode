36bc9211cfec7758910e1aa776068a6b14d9a831
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-17 15:06:02 +0100

    303. Range Sum Query - Immutable

---
+++ 303_RangeSumQueryImmutable/Program.cs
@@ -0,0 +1,25 @@
+﻿// https://leetcode.com/problems/range-sum-query-immutable/description/
+
+
+public class NumArray
+{
+    private readonly int[] _numsSum;
+    
+    public NumArray(int[] nums)
+    {
+        _numsSum = new int[nums.Length+1];
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            
+            _numsSum[i+1] =nums[i]+_numsSum[i]; ;
+        }
+        
+    }
+
+    public int SumRange(int left, int right)
+    {
+        return _numsSum[right+1] - _numsSum[left];
+    }
+}
+
