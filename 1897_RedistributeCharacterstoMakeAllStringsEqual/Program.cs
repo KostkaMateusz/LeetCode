027b62ea9916315de44009db98ff5471a7a1269d
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-19 12:41:17 +0100

    1624. Largest Substring Between Two Equal Characters

---
+++ 1897_RedistributeCharacterstoMakeAllStringsEqual/Program.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/description/
+
+var sol = new Solution();
+
+sol.MakeEqual(["abc", "aabc", "bc"]);
+
+public class Solution
+{
+    public bool MakeEqual(string[] words)
+    {
+        Dictionary<char, int> characterCount = [];
+
+        foreach (var word in words)
+        {
+            foreach (var character in word)
+            {
+                if (!characterCount.TryAdd(character, 1))
+                {
+                    characterCount[character]++;
+                }
+            }
+        }
+
+        foreach (var count in characterCount.Values)
+        {
+            if (count % words.Length != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
