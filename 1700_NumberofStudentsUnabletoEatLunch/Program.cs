dff704d484f2ec380e9a3ff38ec26477b902efe7
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-20 16:06:52 +0100

    1700. Number of Students Unable to Eat Lunch

---
+++ 1700_NumberofStudentsUnabletoEatLunch/Program.cs
@@ -0,0 +1,31 @@
+﻿// https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/description/
+
+
+public class Solution
+{
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        Dictionary<int, int> studentTypeCount = new()
+        {
+            [1] = 0,
+            [0] = 0
+        };
+
+        foreach (var s in students)
+        {
+            studentTypeCount[s]++;
+        }
+
+        for (int i = 0; i < sandwiches.Length; i++)
+        {
+            if (studentTypeCount[sandwiches[i]] == 0)
+            {
+                return students.Length - i;
+            }
+
+            studentTypeCount[sandwiches[i]]--;
+        }
+
+        return 0;
+    }
+}
