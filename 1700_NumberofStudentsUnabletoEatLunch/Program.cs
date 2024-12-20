adfc99e5e653a92a86b2d83b2b0924c6703275f3
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-20 17:10:09 +0100

    496. Next Greater Element I

--- 1700_NumberofStudentsUnabletoEatLunch/Program.cs
+++ 1700_NumberofStudentsUnabletoEatLunch/Program.cs
@@ -5,11 +5,8 @@ public class Solution
 {
     public int CountStudents(int[] students, int[] sandwiches)
     {
-        Dictionary<int, int> studentTypeCount = new()
-        {
-            [1] = 0,
-            [0] = 0
-        };
+        int[] studentTypeCount = new int[2];
+
 
         foreach (var s in students)
         {
