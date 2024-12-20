da103bd94ed80055c07edf6b62f5ef79296c0376
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-20 13:47:15 +0100

    349. Intersection of Two Arrays

---
+++ 349_IntersectionofTwoArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿// https://leetcode.com/problems/intersection-of-two-arrays/
+
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var num1 = new HashSet<int>(nums1);
+        var num2 = new HashSet<int>(nums2);
+
+        return num1.Intersect(num2).ToArray();
+    }
+}
