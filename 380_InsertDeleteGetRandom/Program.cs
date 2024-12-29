b3b16aacb5dffc87c9893c7c4ec73571309fc4ff
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-29 13:11:08 +0100

    380. Insert Delete GetRandom O(1)

--- 380_InsertDeleteGetRandom/Program.cs
+++ 380_InsertDeleteGetRandom/Program.cs
@@ -3,27 +3,55 @@
 
 public class RandomizedSet
 {
-    private readonly HashSet<int> _set;
+    private readonly Dictionary<int, int> _dict;
+    private readonly List<int> _list;
     private readonly Random _random;
 
     public RandomizedSet()
     {
-        _set = new HashSet<int>();
-        _random = new Random();
+        _dict = [];
+        _list = [];
+        _random = new();
     }
 
     public bool Insert(int val)
     {
-        return _set.Add(val);
+        bool contains = _dict.ContainsKey(val);
+
+        if (contains is false)
+        {
+            _list.Add(val);
+            _dict[val] = _list.Count - 1;
+        }
+
+        return !contains;
     }
 
     public bool Remove(int val)
     {
-        return _set.Remove(val);
+        bool contains = _dict.ContainsKey(val);
+
+        if (contains is true)
+        {
+            _dict.Remove(val, out var removedIndex);
+
+            var lastIndex = _list.Count - 1;
+
+            if (removedIndex != lastIndex)
+            {
+                var lastNumber = _list[lastIndex];
+                _list[removedIndex] = lastNumber;
+                _dict[lastNumber] = removedIndex;
+            }
+
+            _list.RemoveAt(lastIndex);
+        }
+
+        return contains;
     }
 
     public int GetRandom()
     {
-        return _set.ElementAt(_random.Next(0, _set.Count) );
+        return _list[_random.Next(_list.Count)];
     }
 }
