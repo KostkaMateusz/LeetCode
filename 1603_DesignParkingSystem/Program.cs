d16d20316ea149a4d3c3f2ec1fc809b769f5242a
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-29 16:51:37 +0100

    1603. Design Parking System

---
+++ 1603_DesignParkingSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://leetcode.com/problems/design-parking-system/description/
+
+public class ParkingSystem
+{
+    private readonly int[] _space = new int[4];
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        _space[1] = big;
+        _space[2] = medium;
+        _space[3] = small;
+    }
+
+    public bool AddCar(int carType)
+    {
+        bool canBeParked = _space[carType] > 0;
+
+        if (canBeParked)
+            _space[carType]--;
+
+        return canBeParked;
+    }
+}
