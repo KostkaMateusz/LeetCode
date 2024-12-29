a8c651e0ee7d5fe1b2d62ae4c8200a3bdb234c19
Mateusz Kostka <mateuszkostka5@gmail.com>
2024-12-29 12:33:50 +0100

    380. Insert Delete GetRandom O(1)

---
+++ 380_InsertDeleteGetRandom/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+
+
+public class RandomizedSet
+{
+    private readonly HashSet<int> _set;
+    private readonly Random _random;
+
+    public RandomizedSet()
+    {
+        _set = new HashSet<int>();
+        _random = new Random();
+    }
+
+    public bool Insert(int val)
+    {
+        return _set.Add(val);
+    }
+
+    public bool Remove(int val)
+    {
+        return _set.Remove(val);
+    }
+
+    public int GetRandom()
+    {
+        return _set.ElementAt(_random.Next(0, _set.Count) );
+    }
+}
