c1177e6d77d835aca00e458afa0b936a01481a3d
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-01-08 19:13:17 +0100

    2405. Optimal Partition of String

---
+++ 2405_OptimalPartitionofString/Program.cs
@@ -0,0 +1,30 @@
+﻿// https://leetcode.com/problems/optimal-partition-of-string/description/
+
+var sol = new Solution();
+
+
+Console.WriteLine(sol.PartitionString("hdklqkcssgxlvehva"));
+
+
+public class Solution
+{
+    public int PartitionString(ReadOnlySpan<char> s)
+    {
+        HashSet<char> group = [];
+
+        int count = 1;
+
+        foreach (var c in s)
+        {
+            if (group.Contains(c))
+            {
+                group.Clear();
+                count++;
+            }
+
+            group.Add(c);
+        }
+        
+        return count;
+    }
+}
