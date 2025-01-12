a20130f5e57a921d53814d8e2ee24d0473234371
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-01-12 20:38:46 +0100

    1291. Sequential Digits

---
+++ 1291_SequentialDigits/Program.cs
@@ -0,0 +1,35 @@
+﻿// https://leetcode.com/problems/sequential-digits/description/
+
+using System.Text;
+
+var sol = new Solution();
+
+sol.SequentialDigits(1000, 13000);
+
+
+public class Solution
+{
+    public IList<int> SequentialDigits(int low, int high)
+    {
+        List<int> result = [];
+
+        for (int seed = 1; seed <= 9; seed++)
+        {
+            int number = seed;
+            int i = seed + 1;
+
+            while (i <= 9)
+            {
+                number = number * 10 + i;
+                i++;
+
+                if (number >= low && number <= high)
+                {
+                    result.Add(number);
+                }
+            }
+        }
+
+        return result.OrderBy(x => x).ToList();
+    }
+}
