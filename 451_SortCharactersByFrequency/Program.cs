93f7ac14f9c6ee1e5541089f47408f65c66d22da
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-01-12 12:15:42 +0100

    51. Sort Characters By Frequency

---
+++ 451_SortCharactersByFrequency/Program.cs
@@ -0,0 +1,33 @@
+﻿// https://leetcode.com/problems/sort-characters-by-frequency/description/
+
+
+using System.Text;
+
+Console.WriteLine();
+
+public class Solution
+{
+    public string FrequencySort(string s)
+    {
+        Dictionary<char, int> frequency = [];
+
+        foreach (var character in s)
+        {
+            if (!frequency.TryAdd(character, 1))
+            {
+                frequency[character] += 1;
+            }
+        }
+
+        StringBuilder result = new();
+
+        var keysInOrder = frequency.OrderByDescending(x => x.Value).Select(x => x.Key);
+
+        foreach (var key in keysInOrder)
+        {
+            result.Append(key, frequency[key]);
+        }
+
+        return result.ToString();
+    }
+}
