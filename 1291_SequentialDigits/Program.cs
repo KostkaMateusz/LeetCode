d79982e014d3a2469c70e9653ef1d27171eec7bd
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-01-20 18:37:24 +0100

    1291. Sequential Digits

--- 1291_SequentialDigits/Program.cs
+++ 1291_SequentialDigits/Program.cs
@@ -6,8 +6,42 @@ var sol = new Solution();
 
 sol.SequentialDigits(1000, 13000);
 
-
 public class Solution
+{
+    public IList<int> SequentialDigits(int low, int high)
+    {
+        IList<int> result = [];
+
+        Queue<int> queue = new([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        while (queue.Count > 0)
+        {
+            int n=queue.Dequeue();
+            if (n > high)
+            {
+                continue;
+            }
+
+            if (n >= low && n <= high)
+            {
+                result.Add(n);
+            }
+            
+            int lastDigit=n%10;
+
+            if (lastDigit < 9)
+            {
+                queue.Enqueue(n*10+(lastDigit+1));
+            }
+        }
+        
+
+        return result;
+    }
+}
+
+
+public class Solution1
 {
     public IList<int> SequentialDigits(int low, int high)
     {
