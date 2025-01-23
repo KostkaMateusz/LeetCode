7a9fec5c2c889edff0e62a19a291142099df86d4
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-01-23 19:55:29 +0100

    2966. Divide Array Into Arrays With Max Difference

---
+++ 2966_DivideArrayIntoArraysWithMaxDifference/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/description/
+
+public class Solution
+{
+    public int[][] DivideArray(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        int[][] result = new int[nums.Length / 3][];
+
+        for (int i = 0, buckerNumber = 0; i < nums.Length; i += 3, buckerNumber++)
+        {
+            if (nums[i + 2] - nums[i] > k)
+            {
+                return new int[][] { };
+            }
+            
+            result[buckerNumber] = new int[3];
+
+            result[buckerNumber][0] = nums[i];
+            result[buckerNumber][1] = nums[i + 1];
+            result[buckerNumber][2] = nums[i + 2];
+        }
+        return result;
+    }
+}
