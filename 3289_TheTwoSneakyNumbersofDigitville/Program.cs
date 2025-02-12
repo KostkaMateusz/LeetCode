01bbe84d0fba7b044662531ec32243dba2163dec
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-02-12 20:34:09 +0100

    3289. The Two Sneaky Numbers of Digitville

---
+++ 3289_TheTwoSneakyNumbersofDigitville/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/description/
+
+
+public class Solution
+{
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        HashSet<int> visited = new();
+
+        int[] duplciate = new int[2];
+        int index = 0;
+
+        foreach (var num in nums)
+        {
+            if (!visited.Add(num) )
+            {
+                duplciate[index] = num;
+                index++;
+            }
+
+            if (index == 2)
+            {
+                break;
+            }
+        }
+
+        return duplciate;
+    }
+}
