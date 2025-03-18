1a88d30d13e1de840bccb4f27aaf21aecda1756b
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-03-18 19:01:31 +0100

    125. Valid Palindrome

---
+++ 125_ValidPalindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿// https://leetcode.com/problems/valid-palindrome/
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        int leftPointer = 0;
+        int rightPointer = s.Length - 1;
+
+        while (leftPointer <= rightPointer)
+        {
+            if (!char.IsLetterOrDigit(s[leftPointer]))
+            {
+                leftPointer++;
+            }
+            else if (!char.IsLetterOrDigit(s[rightPointer]))
+            {
+                rightPointer--;
+            }
+            else if (char.ToLower(s[leftPointer]) != char.ToLower(s[rightPointer]))
+            {
+                return false;
+            }
+            else
+            {
+                leftPointer++;
+                rightPointer--;
+            }
+        }
+
+        return true;
+    }
+}
