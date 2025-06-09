a804ab5e9bc520eb90ede5c29b18601c4474b9e1
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-06-09 23:31:37 +0200

    2678. Number of Senior Citizens

---
+++ 2678_NumberofSeniorCitizens/Program.cs
@@ -0,0 +1,35 @@
+﻿// https://leetcode.com/problems/number-of-senior-citizens/description/
+
+
+var sol = new Solution();
+
+sol.CountSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"]);
+
+public class Solution
+{
+    public int CountSeniors(string[] details)
+    {
+
+        int over60 = 0;
+
+        foreach (var str in details)
+        {
+
+            var firstDigit = str[11];
+
+            if (firstDigit > '6')
+            {
+                over60++;
+            }
+            else if (firstDigit == '6')
+            {
+                if (str[12] > '0')
+                {
+                    over60++;
+                }
+            }
+
+        }
+        return over60;
+    }
+}
