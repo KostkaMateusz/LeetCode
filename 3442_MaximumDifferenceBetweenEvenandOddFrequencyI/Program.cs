33a7926cce89f00ca9bb36efeb95eee993868843
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-07-08 22:50:18 +0200

    3442. Maximum Difference Between Even and Odd Frequency I

---
+++ 3442_MaximumDifferenceBetweenEvenandOddFrequencyI/Program.cs
@@ -0,0 +1,42 @@
+﻿// https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/
+
+var sol = new Solution();
+
+sol.MaxDifference("mmsmsym");
+
+
+public class Solution
+{
+    public int MaxDifference(string s)
+    {
+
+        int[] counts = new int[26];
+
+        foreach (char character in s)
+        {
+            counts[character - 'a'] += 1;
+        }
+
+
+        int maxOdd = Int32.MinValue;
+        int minEven = Int32.MaxValue;
+
+
+        foreach (int c in counts)
+        {
+            if (c != 0)
+            {
+                if (c % 2 == 0)
+                {
+                    minEven = Math.Min(c, minEven);
+                }
+                else
+                {
+                    maxOdd = Math.Max(c, maxOdd);
+                }
+            }
+        }
+        return maxOdd - minEven;
+
+    }
+}
