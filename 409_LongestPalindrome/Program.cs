1b31ffff7440452e79e92071d7f27b887f0a7bfe
Mateusz Kostka <mateuszkostka5@gmail.com>
2025-07-31 14:30:49 +0200

    409. Longest Palindrome

---
+++ 409_LongestPalindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿// https://leetcode.com/problems/longest-palindrome/description/
+
+public class Solution
+{
+    public int LongestPalindrome(string s)
+    {
+        Dictionary<char, int> charCount = new();
+
+        foreach (char c in s)
+        {
+            if (!charCount.TryAdd(c, 1))
+            {
+                charCount[c] += 1;
+            }
+        }
+
+
+        int length = 0;
+        bool hasOdd = false;
+
+        foreach (var count in charCount.Values)
+        {
+            length += count / 2 * 2;
+            if (count % 2 == 1)
+            {
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd)
+        {
+            length += 1;
+        }
+
+        return length;
+
+    }
+}
