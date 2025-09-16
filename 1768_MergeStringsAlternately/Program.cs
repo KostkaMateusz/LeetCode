6539b2d7201498ea8404888b85886ec853743fcf
unknown <mateuszkostka5@gmail.com>
2025-09-16 13:15:43 +0200

    1768. Merge Strings Alternately

---
+++ 1768_MergeStringsAlternately/Program.cs
@@ -0,0 +1,38 @@
+﻿// https://leetcode.com/problems/merge-strings-alternately/
+
+var sol = new Solution();
+
+sol.MergeAlternately(word1: "abc", word2: "pqr");
+
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        int pointer1 = 0;
+        int pointer2 = 0;
+        int mergePointer = 0;
+        char[] mergeString = new char[word1.Length + word2.Length];
+
+        while (mergePointer != word1.Length + word2.Length)
+        {
+            if (pointer1 < word1.Length)
+            {
+                mergeString[mergePointer] = word1[pointer1];
+                mergePointer++;
+                pointer1++;
+            }
+
+            if (pointer2 < word2.Length)
+            {
+                mergeString[mergePointer] = word2[pointer2];
+                mergePointer++;
+                pointer2++;
+            }
+
+        }
+       
+        return new string(mergeString);
+
+    }
+}
