24c9489b4b82b393c37bec706fb1ef771cb9293a
Mateusz <mateuszkostka5@gmail.com>
2025-09-16 14:31:19 +0200

    455. Assign Cookies

---
+++ 455_AssignCookies/Program.cs
@@ -0,0 +1,22 @@
+﻿https://leetcode.com/problems/assign-cookies/
+
+public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        Array.Sort(g);
+        Array.Sort(s);
+        int contentChild = 0;
+
+        for (int cookie = 0; contentChild < g.Length && cookie < s.Length; cookie++)
+        {
+            if (g[contentChild] <= s[cookie])
+            {
+                contentChild++;
+            }
+        }
+
+        return contentChild;
+
+    }
+}
