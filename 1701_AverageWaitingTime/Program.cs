dd736e2abfe050c5c2372955500bf33e7ead6577
Mateusz <mateuszkostka5@gmail.com>
2025-09-26 17:02:42 +0200

    1701. Average Waiting Time_Dev

---
+++ 1701_AverageWaitingTime/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.com/problems/average-waiting-time/
+
+public class Solution
+{
+    public double AverageWaitingTime(int[][] customers)
+    {
+        int currentTime = 1;
+        int[] customerWaitTaime = new int[customers.Length];
+
+        for (int i = 0; i < customers.Length; i++)
+        {
+            int[] customer = customers[i];
+
+            if (customer[0] >= currentTime)
+            {
+                currentTime = customer[0];
+            
+            
+                currentTime += customer[1];
+
+
+            }
+
+
+        }
+
+    }
+}
